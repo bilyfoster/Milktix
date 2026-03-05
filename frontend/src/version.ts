2cda67ea54e503d90a38b2d68419c1dcadc1bfd0
Jenkins <jenkins@xavier.trex-skate.ts.net>
2026-03-05 20:36:54 +0100

    fix: update version to 1.2.0

--- frontend/src/version.ts
+++ frontend/src/version.ts
@@ -2,7 +2,7 @@
 // Generated: Thu Mar  5 12:19:05 CET 2026
 // Change VERSION in update-version.sh for releases
 
-export const VERSION = '1.1.1';
+export const VERSION = '1.2.0';
 export const BUILD_DATE = '2026-03-05';
 export const GIT_COMMIT = '963cd5f';
 
